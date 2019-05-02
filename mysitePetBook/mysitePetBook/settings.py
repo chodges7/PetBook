7646ba0cbe1080831e5a20aef79779ad13716576
chodges7 <cdhodges5@gmail.com>
2019-05-02 07:27:33 +0000

    Changed gitignore so .json file is ignored

--- mysitePetBook/mysitePetBook/settings.py
+++ mysitePetBook/mysitePetBook/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from google.oauth2 import service_account
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,5 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django google storage
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = 'petbook-bucket'
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    "/code/mysitePetBook/PetBook.json"
+)
 
 LOGIN_REDIRECT_URL = '/'
