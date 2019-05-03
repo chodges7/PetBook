75cacc94994a9949e09e369e782091e852053b8f
chodges7 <cdhodges5@gmail.com>
2019-05-03 20:31:27 +0000

    Fixed pylint issue in pipeline

--- pylint.sh
+++ pylint.sh
@@ -1,2 +1,3 @@
 pylint --load-plugins pylint_django --disable=missing-docstring ./mysitePetBook/mysitePetBook
 pylint --load-plugins pylint_django --disable=missing-docstring ./mysitePetBook/myapp
+pylint --load-plugins pylint_django --disable=missing-docstring ./mysitePetBook/Chat
